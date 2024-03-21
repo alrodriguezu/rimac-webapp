40ba5dd9929811b77ba5a22d3d738482ac528834
alrodriguezu <anyirodriguezu.com>
2024-03-20 22:13:56 -0500

    fix(004): correct styles

--- src/components/stepper/stepper.tsx
+++ src/components/stepper/stepper.tsx
@@ -11,10 +11,27 @@ const Stepper: React.FC<IStepperProps> = ({ active, image }) => {
       <div className="stepper__container">
         <div className="stepper__container__step">
           <div className="stepper__container__step__pos">
+            <button
+              type="button"
+              className="btn stepper__container__step__pos--mobile"
+              onClick={() => {}}
+            >
+              <div className="plans__container__content__back__icon">
+                <img
+                  className="plans__container__content__back__icon__image"
+                  src="./icon-back.svg"
+                />
+              </div>
+            </button>
+            <div className="text--on">PASO 1 DE 2</div>
             <div className={`icon ${active != 1 && 'icon--off'}`}>1</div>
             <div className={`text ${active != 1 && 'text--off'}`}>Planes y coberturas</div>
           </div>
-          <img src={image} />
+          <img className="stepper__container__step__pos__image" src={image} />
+          <img
+            className="stepper__container__step__pos__image--mobile"
+            src="./atoms_stepper_progress.svg"
+          />
         </div>
         <div className="stepper__container__step">
           <div className="stepper__container__step__pos">
